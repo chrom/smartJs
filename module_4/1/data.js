51aedcb1bfca432c485d89f542b0f614dc24a9aa
Andrey Zabara <dr.chrom@gmail.com>
2016-02-20 17:31:38 +0200

    commit 4 1

--- module_4/1/data.js
+++ module_4/1/data.js
@@ -1,49 +1,49 @@
-
-
-function Complex(base){
-    this.base = (typeof base === 'number') ? {x:base, y:0} : base;
-    this.add = function(obj){
-        var result;
-        switch (typeof this.base) {
-            case 'number':
-                if (typeof obj === 'number'){
-                    result = add2int();
-                } else {
-                    result = addIntComplex(this.base, obj);
-                }
-                break;
-            case 'object':
-                if (typeof obj === 'number'){
-                    result = addIntComplex(obj, this.base);
-                } else {
-                    result = add2Complex();
-                }
-                break;
-        }
-
-        function add2int(){ return {x:base + obj, y:0}; };
-        function addIntComplex(int, complex){return {x:int + complex.x, y:complex.y}};
-        function add2Complex(){return {x:base.x + obj.x, y:base.y + obj.y}};
-        return result;
+function Complex(base) {
+    this.add = function (obj) {
+        return {x: base.x + obj.x, y: base.y + obj.y};
+        //(a,b)+(c,d)=(a+b,c+d);
     };
-    this.sub = function(obj){
-        var result;
-        if (typeof obj === 'number'){
-            obj = {x:obj, y:0};
-        }
-        result = {x:this.base.x-obj.x, y:this.base.y-obj.y};
-
+    this.sub = function (obj) {
+        obj = this.clearObj(obj);
+        return {x: this.base.x - obj.x, y: this.base.y - obj.y};
         //(a,b)-(c,d)=(a-b,c-d);
-        return result;
     };
-    this.mul = function(){
+    this.mul = function (obj) {
+        obj = this.clearObj(obj);
+        return {x: this.base.x * obj.x - this.base.y * obj.y, y: this.base.x * obj.y + this.base.y * obj.x};
         //(a,b)x(c,d)=(ac-bd,ad+bc);
-
-
     };
-    this.div = function(){};
-    this.equ = function(){};
-    this.conj = function(){};
+    this.div = function (obj) {
+        obj = this.clearObj(obj);
+        var result = {
+            x: (this.base.x * obj.x + this.base.y * obj.y) / (obj.x * obj.x),
+            y: (this.base.x * obj.y + this.base.y * obj.x) / (obj.y * obj.y)
+        };
+        return result;
+        //(a,b)/(c,d)=(ac+bd,bc-ad)/(c^2+d^2);
+    };
+    this.equ = function (obj) {
+        obj = this.clearObj(obj);
+        return this.base.x === obj.x && this.base.y === obj.y;
+        //(a,b)=(c,d),если (a=c) и (b=d);
+    };
+    this.conj = function (obj) {
+        obj = this.clearObj(obj);
+        return {x: obj.x, y: Math.abs(obj.y)};
+        //conj (a,b)=(a,-b).
+    };
+    this.clearObj = function (obj) {
+        var stack = [obj, this.base];
+        for (var item in stack) {
+            for (var index in stack[item]) {
+                if (stack[item].hasOwnProperty(index) && isNaN(stack[item][index])) {
+                    throw new Error('Some value is NaN');
+                }
+            }
+        }
+        return (typeof obj === 'number') ? {x: obj, y: 0} : obj;
+    };
+    this.base = this.clearObj(base);
 }
-    var complex = new Complex(10);
-    console.log(complex.sub(8));
+var complex = new Complex({x: 2, y: 2});
+console.log(complex.conj({x: 2, y: -2}));
