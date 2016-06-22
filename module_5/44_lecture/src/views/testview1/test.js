a98a9b0d1518db75070654ec2482945fad847fc3
slava boiarchuk <falanor@mail.ru>
2016-06-22 16:34:16 +0300

    remove files

--- module_5/44_lecture/src/views/testview1/test.js
+++
@@ -1,6 +0,0 @@
-export RegistrationForm from './RegistrationForm/RegistrationForm';
-export LoginForm from './LoginForm/LoginForm';
-export ConfirmPopup from './ConfirmPopup/confirmPopup';
-export UserBoard from './UsersBoard/UserBoard';
-export NavView from './NavView/NavView';
-export UserView from './UserView/UserView';
